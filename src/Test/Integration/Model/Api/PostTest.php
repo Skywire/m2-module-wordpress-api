ff071a0bd08218a0dda924aa84c605d1b580001e
Neil Aitken <neil@skywire.co.uk>
2018-09-13 11:49:24 +0100

    1.1.0 Release
    Add tests

---
+++ src/Test/Integration/Model/Api/PostTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Skywire\WordpressApi\Model\Api;
+
+
+use Skywire\WordpressApi\Test\Integration\Model\Api\AbstractApiTest;
+
+/**
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Skywire License
+ * that is available through the world-wide-web at this URL:
+ * http://skywire.co.uk/skywire/license/
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to interact@skywire.co.uk so we can send you a copy immediately.
+ *
+ */
+
+/**
+ * Description of class
+ *
+ * Further description is required
+ *
+ * @category             Skywire
+ * @package              Skywire\ModuleName
+ * @author               Skywire Tech Team <tech@skywire.co.uk>
+ * @copyright            2018 Skywire Ltd (http://skywire.co.uk)
+ * @license              Skywire Licence http://skywire.co.uk/skywire/license/
+ * @link                 http://skywire.co.uk
+ */
+class PostTest extends AbstractApiTest
+{
+    public function testGetCollection()
+    {
+        $post = $this->objectManager->get(Post::class);
+        $result   = $post->getCollection();
+
+        $this->assertCount(10, $result);
+        $this->assertEquals(1, $result->getFirstItem()->getId());
+    }
+
+    public function testGetEntity()
+    {
+        $post = $this->objectManager->get(Post::class);
+        $result   = $post->getEntity(1);
+
+        $this->assertEquals(1, $result->getId());
+    }
+}
