2699039e39221dacba6e1144759f6c9eeb38452a
Neil <neil@skywire.co.uk>
2020-11-11 09:32:47 +0000

    DD#0000: feat: add title accessor method to path interface

--- src/Api/Data/Index/PathInterface.php
+++ src/Api/Data/Index/PathInterface.php
@@ -9,6 +9,8 @@ namespace Skywire\WordpressApi\Api\Data\Index;
  * @method self setPath(string $path);
  * @method string getSlug();
  * @method self setSlug(string $slug);
+ * @method string getTitle();
+ * @method self setTitle(string $title);
 *
  */
 interface PathInterface
